2265888265e66757107764332ab7ef67d2045f9e
ahofman <adrian@adrianhofman.com>
2014-04-25 11:02:39 +1000

    Added a test fixture to the test project that includes a skipped test. Added a pubdate time element.

---
+++ Tests/SkippedTestFixture.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestFixture]
+    public class SkippedTestFixture
+    {
+        [Test]
+        public void ThisTestPasses()
+        {
+            Assert.AreEqual(2, 2);
+        }
+
+        [Test]
+        [Ignore("For some reason this wont work so im ignoring it")]
+        public void ThisTestPassesButIsIgnored()
+        {
+            Assert.AreEqual(2, 2);
+        }
+    }
+}
